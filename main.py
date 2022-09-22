dd21d75b3fef1caadbe64487897f18bd54439598
senchi <senchpimy@gmail.com>
2022-09-22 11:51:46 -0500

    recursive

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import os 
 import hashlib
-import sys
+from os.path import isdir
+from pathlib import Path
 import time 
 #################################################################################################################################
-#t = time.localtime()
-#current_time = time.strftime("%H:%M:%S", t)
-#print(current_time)
-#################################################################################################################################
-def init(): #creacion de la base de datos
+def init(): #creacion de la base de datos (completado)
     if os.path.exists(".pbm"):
         print("Database already exists")
     else:
@@ -21,28 +18,33 @@ def ignore():
     if os.path.exists(".pbmignore"):
         1+1
 #################################################################################################################################
-def hash_dir(dir,dirs_dict): #Hashea todos los elementos de un directorio y regresa los hashes
+def hash_dir(dir): #Hashea todos los elementos de un directorio y regresa los hashes
     current_dir_hashes=list()
     current_dir_names=list()
-    for i in os.lisdir():
+    for i in os.listdir(dir):
         if os.path.isdir(i):
             continue
         else:
-            cuurent_dir_hashes.append(hash_file(i))
+            current_dir_hashes.append(hash_file(i))
             current_dir_names.append(i)
+    return current_dir_names,current_dir_hashes
 #################################################################################################################################
 def hash_file(file): #Da el hash de el archivo (completado)
     BUF_SIZE = 65536 #El tamaño en el que el archivo se va adividir
-    file=open(i,"rb") #abrimos el archivo para leer sus bytes
+    try:
+        file_o=open(file,"rb") #abrimos el archivo para leer sus bytes
+    except:
+        #return hashlib.sha512(str("0").encode("utf-8"))
+        return "0"
     sha512=hashlib.sha512() #creamos un obejeto en el que se guardara la shasum
     while True: #Leemos el archivo hast aque ya no halla info 
-        data = file.read(BUF_SIZE)#Leemos el archivos en cachitos
+        data = file_o.read(BUF_SIZE)#Leemos el archivos en cachitos
         if not data: #Se acaba el loop cuando ya no hay mas bytes en el archivo
             break
         sha512.update(data) #Actualizamos la shasum de el archivo que estamos leyendo con el nuevo chachito que obtivumos
         return sha512
 #################################################################################################################################
-def formater(files,hashes,dire="."): #Lee los nombres de los archivos y hashes y los guarda en .pbm (completado)
+def formater(files,hashes): #Lee los nombres de los archivos y hashes y los guarda en .pbm (completado)
     len_hash=len(hashes) 
     len_list=len(files)
     if len_hash!=len_list: #Nos aseguramos que haya igual de hashes que de elementos
@@ -54,10 +56,23 @@ def formater(files,hashes,dire="."): #Lee los nombres de los archivos y hashes y
 
     database=open(".pbm","a")#Abrimos la base de datos
     for i in range(len_hash): #pasamos por cada elemento de nustras listas
-       database.write(dire+"/"+files[i]+"   "+hashes[i]+"\n") #escribimos el archivo y su hash
+        try:
+            database.write(files[i]+"   "+hashes[i].hexdigest()+"\n") #escribimos el archivo y su hash
+        except:
+            database.write(files[i]+"   imposible de hashear\n") #escribimos el archivo y su hash
     database.close() #cerramos el archivo
+#################################################################################################################################
+def recursive(path): #
+    listu=list(Path(path).rglob("*"))
+    files=[]
+    hashes=[]
+    for i in listu:
+        files.append("./"+str(i))
+        hashes.append(hash_file("./"+str(i)))
+    formater(files,hashes)
 #################################################################################################################################
 #t2 = time.localtime()
 #current_time2 = time.strftime("%H:%M:%S", t2)
 #print(current_time2)
+recursive(".")
 #################################################################################################################################
