750b6c60cc0190a4092f01cd31e5edad2bf290bd
senchi <senchpimy@gmail.com>
2022-10-18 16:31:06 -0500

    avances y funciones

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import hashlib
 from os.path import isdir
 from pathlib import Path
 import time
-from typing import List 
+import sys
 #################################################################################################################################
 def init(): #creacion de la base de datos (completado)
     if os.path.exists(".pbm"):
@@ -53,6 +53,21 @@ def hash_file(file): #Da el hash de el archivo (completado)
         sha512.update(data) #Actualizamos la shasum de el archivo que estamos leyendo con el nuevo chachito que obtivumos
         return sha512
 #################################################################################################################################
+def hash_file_special(file): #Por algun motivo no queria aplicar la propiedad .hexdigest() a la salida de esta funcion por que la detectaba como str
+    BUF_SIZE = 65536 #El tamaño en el que el archivo se va adividir
+    try:
+        file_o=open(file,"rb") #abrimos el archivo para leer sus bytes
+    except:
+        #return hashlib.sha512(str("0").encode("utf-8"))
+        return "0"
+    sha512=hashlib.sha512() #creamos un obejeto en el que se guardara la shasum
+    while True: #Leemos el archivo hast aque ya no halla info 
+        data = file_o.read(BUF_SIZE)#Leemos el archivos en cachitos
+        if not data: #Se acaba el loop cuando ya no hay mas bytes en el archivo
+            break
+        sha512.update(data) #Actualizamos la shasum de el archivo que estamos leyendo con el nuevo chachito que obtivumos
+        return sha512.hexdigest()
+#################################################################################################################################
 def formater(files,hashes): #Lee los nombres de los archivos y hashes y los guarda en .pbm (completado)
     len_hash=len(hashes) 
     len_list=len(files)
@@ -98,43 +113,99 @@ def GetDataBase(): #Lee el archivo .pbm y regresa un alista de tuplas con el pat
     return databse
 
 #################################################################################################################################
+def GetDataBaseNames(): #Lee el archivo .pbm y regresa una lista de los nombres de los archivos (completado)
+    databse=list()
+    file=open(".pbm","r")
+    for h in file.read().splitlines():
+        i,_=h.split("▓¥»╚▓¥»╚")
+        databse.append(i)
+    return databse
+
+#################################################################################################################################
 def Compare(): #Obtiene los archvos que han sido modificados entre el .pbm y la carpeta y los regresa en una lista (completado)
     database=GetDataBase()
     ArchivosDiferentes=list()
     for i in database:
-        #print(hash_file(i[0]).hexdigest())
         if i[1]=="imposible_de_hashear":
             continue
-        if i[1]!=hash_file(i[0]).hexdigest():
-#            print("diferentes")
-#            print(i[0])
-#            print(hash_file(i[0]).hexdigest())
-             ArchivosDiferentes.append(i[0])  
+        if i[1]!=hash_file_special(i[0]):
+             ArchivosDiferentes.append(i[0])
     return ArchivosDiferentes
 #################################################################################################################################
-def EliminatedFiles(): #Busca los archivos que estaban en la database pero ya no estan en el directorio
-    Database=GetDataBase()
+def DeletedAndUntracked():
+    Database=GetDataBaseNames()
+    CurrenteFiles=list()
     listu=list(Path(".").rglob("*"))
-    eliminatedFiles=list()
 
     for i in listu:
-        i="./"+str(i)
         if isdir(i):
             continue
-        print(i)
-#        if i in Database:
-#            continue
-#        else:
-#            eliminatedFiles.append(i)
-#    
-#################################################################################################################################
-def getAllFiles(lista):
-    listu=list(Path(".").rglob("*"))
-    for i in listu:
-        i="./"+str(i)
-        if isdir(i):
-            continue
+        CurrenteFiles.append("./"+str(i))
+
+    Deleted=[x for x in Database if x not in CurrenteFiles]
+    untracked=[x for x in CurrenteFiles if x not in Database]
+    return Deleted, untracked
 
 #################################################################################################################################
+def status():
+    Deleted, untracked=DeletedAndUntracked()
+    ArvhivosDiferentes=Compare()
 
-print(EliminatedFiles())
+    deletedlen=len(Deleted)
+    Diferenteslen=len(ArvhivosDiferentes)
+    Untrackedlen=len(untracked)
+
+    if deletedlen==0:
+        print("No files deleted")
+    else:
+        print(f"{deletedlen} files were deleted\033[1m")
+        for i in Deleted:
+            print(i)
+        print("\033[0m")
+
+    if Untrackedlen==0:
+        print("No untracked files")
+    else:
+        print(f"{Untrackedlen} files are untracked \033[1m")
+        for i in untracked:
+            print(i)
+        print("\033[0m")
+
+    if Diferenteslen==0:
+        print("No files changed")
+    else:
+        print(f"{Diferenteslen} files were changed\033[1m")
+        for i in ArvhivosDiferentes:
+            print(i)
+        print("\033[0m")
+#################################################################################################################################
+def Use():
+    print('\033[1m'+"PBM is a git-like backups manager"+'\033[0m\n')
+    print('\033[91m'+"init                  "+'\033[0m'+"Starts a Database")
+    print('\033[91m'+"recursive             "+'\033[0m'+"Adds all the files recursively")
+    print('\033[91m'+"update                "+'\033[0m'+"Update the database")
+    print('\033[91m'+"copy \033[92m\033[4m\x1B[3m*location*"+'\033[0m       '+"Copy the files in the database to a specified location")
+    print('\033[91m'+"add                   "+'\033[0m'+"Add a file to the database")
+    print('\033[91m'+"status                "+'\033[0m'+"Get the status of the database")
+    print('')
+
+#################################################################################################################################
+def main():
+
+    if len(sys.argv)==1:
+        Use()
+    elif sys.argv[1]=="init": 
+        init()
+    elif sys.argv[1]=="recursive": 
+        recursive(".")
+    elif sys.argv[1]=="status": 
+        status()
+    else:
+        print("Invalid option\n")
+        Use()
+
+#################################################################################################################################
+#if __name__ == "__main__":
+#                main()
+status()
+#print(Compare())
